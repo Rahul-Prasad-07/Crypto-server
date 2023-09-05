1d6bd79e0b6419590769b7e3ebe6ac583bb1b8f0
Rahul-Prasad-07 <prasadrahulprn3@gmail.com>
2023-09-05 15:24:20 +0530

    Able to get cryto data via api

--- server/index.js
+++ server/index.js
@@ -1,8 +1,28 @@
+require("dotenv").config();
+const { default: axios } = require("axios");
 const express = require("express");
 const app = express();
 app.use(express.json());
 
 const PORT = 3000;
+
+const api = axios.create({
+  method: "GET",
+  baseURL: "https://pro-api.coinmarketcap.com/v1/cryptocurrency",
+  headers: {
+    "X-CMC_PRO_API_KEY": process.env.COINMARKETCAP_API,
+    Accept: "application/json",
+    "Content-Encoding": "deflate, gzip",
+  },
+});
+
+// Top 10
+app.get("/TopTen", (req, res) => {
+  api("/listings/latest?limit=10")
+    .then((response) => response.data)
+    .then((value) => res.json(value.data))
+    .catch((err) => console.log(err));
+});
 
 // define routes
 app.get("/status", (req, res) => {
