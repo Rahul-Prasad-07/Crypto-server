c2bee17da8e3c7d1498bd58246597af7fb7559c0
Rahul-Prasad-07 <prasadrahulprn3@gmail.com>
2023-09-05 18:18:04 +0530

    Able to get symbol and price

--- server/routes/CryptoCurrency.js
+++ server/routes/CryptoCurrency.js
@@ -26,4 +26,58 @@ router.get("/TopTen", async (req, res) => {
   }
 });
 
+// routes.js
+
+// ... (Previous code)
+
+// Define a route for a specific cryptocurrency by symbol
+router.get("/:symbol", async (req, res) => {
+  const symbol = req.params.symbol;
+
+  try {
+    // Fetch cryptocurrency data by symbol
+    const response = await api.get(`/quotes/latest?symbol=${symbol}`);
+
+    if (response.data && response.data.data && response.data.data[symbol]) {
+      const cryptoData = response.data.data[symbol];
+      res.json(cryptoData);
+    } else {
+      res.status(404).json({ error: "Cryptocurrency not found" });
+    }
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: "An error occurred" });
+  }
+});
+
+router.get("/:symbol/price", async (req, res) => {
+  const symbol = req.params.symbol;
+
+  try {
+    // Fetch cryptocurrency data by symbol
+    const response = await api.get(`/quotes/latest?symbol=${symbol}`);
+
+    if (response.data && response.data.data && response.data.data[symbol]) {
+      const cryptoData = response.data.data[symbol];
+      if (
+        cryptoData.quote &&
+        cryptoData.quote.USD &&
+        cryptoData.quote.USD.price
+      ) {
+        const priceInUSD = cryptoData.quote.USD.price;
+        res.json({ priceUSD: priceInUSD });
+      } else {
+        res
+          .status(404)
+          .json({ error: "Price not found for the cryptocurrency" });
+      }
+    } else {
+      res.status(404).json({ error: "Cryptocurrency not found" });
+    }
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: "An error occurred" });
+  }
+});
+
 module.exports = router;
